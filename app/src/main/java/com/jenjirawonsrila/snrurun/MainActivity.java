4ae35398fd52972957e66a5e112f2bbe4398cb49
jane <nareewongsrila@gmail.com>
2016-05-17 19:04:20 -0700

    Picasso

--- app/src/main/java/com/jenjirawonsrila/snrurun/MainActivity.java
+++ app/src/main/java/com/jenjirawonsrila/snrurun/MainActivity.java
@@ -2,19 +2,40 @@ package com.jenjirawonsrila.snrurun;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private ImageView imageView ;
+    private EditText userEditText,passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText)findViewById(R.id.editText4);
+        passwordEditText =(EditText)findViewById(R.id.editText5);
 
         myManage = new MyManage(MainActivity.this);
 
@@ -24,8 +45,74 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         deleteAllSQLite();
 
+        //Synchronize
+        MySynchronize mySynchronize = new MySynchronize();
+        mySynchronize.execute();
+
+        //show
+
+        Picasso.with(MainActivity.this)
+                .load("http://swiftcodingthai.com/snru/image/logo_snru.png")
+                .resize(200,250)
+                .into(imageView);
+
 
     }//Main Method
+
+
+    //Create Inner Class
+    public class MySynchronize extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient  =new OkHttpClient();
+                Request.Builder builder =new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/snru/get_user.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            }catch (Exception e){
+                return null;
+
+            }
+
+            //return null;
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("Snru", "JSON=="+s);
+
+            try{
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++){
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(MyManage.column_name);
+                    String strUser = jsonObject.getString(MyManage.column_user);
+                    String strPassword = jsonObject.getString(MyManage.column_password);
+                    String strAvata = jsonObject.getString(MyManage.column_avata);
+
+                    myManage.addUser(strName,strUser,strPassword,strAvata);
+
+                }//for
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }//onPost
+
+
+    }//MySyn Class
+
+
 
     private void deleteAllSQLite() {
 
